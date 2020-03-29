55d6b93da2c468147ab64f513ddde16f37686ad7
Renato Gobet Uzun <renato.t0771.uzun@xpi.com.br>
2020-03-29 20:30:06 -0300

    Adding MD5Hash Calculator to Common Utils

---
+++ UzunTec.Utils.Common/MD5Hash.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace UzunTec.Utils.Common
+{
+    public static class MD5Hash
+    {
+        public static string CalculateMD5Hash(this string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+                byte[] hash = md5.ComputeHash(inputBytes);
+
+                // Segundo passo, converter o array de bytes em uma string haxadecimal
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hash.Length; i++)
+                {
+                    sb.Append(hash[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
+
