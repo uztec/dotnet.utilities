8a7247b1e5c61fe32a6d69adbb381e28380508be
Renato Gobet Uzun <renato.t0771.uzun@xpi.com.br>
2020-03-29 17:00:48 -0300

    Adding Data Results instead of DataRow / DataTable

---
+++ UzunTec.Utils.DatabaseAbstraction/DataResulRecord.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace UzunTec.Utils.DatabaseAbstraction
+{
+    public class DataResulRecord : Dictionary<string, object>, IDictionary<string, object>, IReadOnlyDictionary<string, object>
+    {
+        public DataResulRecord(IDataReader reader)
+        {
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                this.Add(reader.GetName(i), reader[i]);
+            }
+        }
+
+        public T? GetNullableValue<T>(string ColumnName) where T : struct
+        {
+            object value = (this[ColumnName] == System.DBNull.Value) ? null : this[ColumnName];
+            return (value == null) ? (T?) null : (T)Convert.ChangeType(value, typeof(T));
+        }
+
+        public T GetValue<T>(string ColumnName) where T : struct
+        {
+            object value = (this[ColumnName] == System.DBNull.Value) ? null : this[ColumnName];
+            return (value == null) ? default(T) : (T)Convert.ChangeType(value, typeof(T));
+        }
+
+        public string GetString(string ColumnName)
+        {
+            return (this[ColumnName] == System.DBNull.Value) ? null : (string)this[ColumnName];
+        }
+    }
+}
---
+++ UzunTec.Utils.DatabaseAbstraction/DataResultTable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UzunTec.Utils.Common;
+
+namespace UzunTec.Utils.DatabaseAbstraction
+{
+    public class DataResultTable : List<DataResulRecord>
+    {
+        public IReadOnlyDictionary<string, Type> Fields { get; }
+
+        private DataResultTable(IDataReader reader)
+        {
+            Dictionary<string, Type> cols = new Dictionary<string, Type>();
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                cols.Add(reader.GetName(i), reader.GetFieldType(i));
+            }
+
+            while (reader.Read())
+            {
+                this.Add(new DataResulRecord(reader));
+            }
+            reader.Close();
+        }
+
+        public void InsertRecord(int index, DataResulRecord record)
+        {
+            if (record.Keys.CompareTo(new List<string>(this.Fields.Keys)) != 0)
+            {
+                throw new ArgumentException("This Record does not contains the same fields of this DataResultTable");
+            }
+            this.Insert(index, record);
+        }
+
+        public void AddRecord(DataResulRecord record)
+        {
+            this.InsertRecord(this.Count, record);
+        }
+    }
+}
